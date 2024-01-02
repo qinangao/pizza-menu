75ba3a2befd9d71d08fc42558284e97982cfe735
Qinan Gao <qinanagao@gmail.com>
2024-01-02 21:55:25 +0000

    add prop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,20 @@ const Footer = () => {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  //   if (hour >= openHour && hour <= closeHour) {
-  //     alert("We are currently open.");
-  //   } else {
-  //     alert("We are currently closed.");
-  //   }
 
   return (
     <footer className="footer ">
-      {new Date().toLocaleTimeString()} We are currently{" "}
-      {isOpen ? "open" : "closed"}.
+      {isOpen ? (
+        <div className="order">
+          <p>We are open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          Sorry, we are currently closed. Welcome to vist us between {openHour}
+          :00 to {closeHour}:00
+        </p>
+      )}
     </footer>
   );
 };
