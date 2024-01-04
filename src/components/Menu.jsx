30ef0f9d738ab9499763058aeb24c14942b9d79e
Qinan Gao <qinanagao@gmail.com>
2024-01-04 21:50:55 +0000

    add fragment

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Pizzas from "./Pizzas";
+import pizzaData from "../data";
 
 const Menu = () => {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const pizzasNum = pizzas.length;
   return (
     <div className="menu">
       <h2>Our Menu</h2>
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from
-        our stone oven. All organic, all delicious
-      </p>
-      <Pizzas />
+
+      {pizzasNum > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven. All organic, all delicious
+          </p>
+          <Pizzas />
+        </>
+      ) : (
+        <p>Sorry, We are still workin on the menu. Please come back later.</p>
+      )}
     </div>
   );
 };
